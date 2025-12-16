d554a225cdfc41faea2e5da86b7bb768b8faef71
Luiz Guilherme <luizgl_jatai@outlook.com>
2025-12-16 18:39:20 -0300

    Puxe

--- Projeto-Avaliativo/src/View/Nota/NotaTabelaView.java
+++ Projeto-Avaliativo/src/View/Nota/NotaTabelaView.java
@@ -1,20 +1,14 @@
 package View.Nota;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.util.List;
 import java.util.Map;
 
 public class NotaTabelaView {
-
     public void mostrarTabela(List<Map<String, String>> notas) {
-
-        // Colunas da tabela
         String[] colunas = {
-            "Id", "Nota", "ID Diário", "Matrícula Aluno"
+                "Id", "Nota", "ID Diário", "Matrícula Aluno"
         };
-
-        // Modelo da tabela (não editável)
         DefaultTableModel model = new DefaultTableModel(colunas, 0) {
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -22,27 +16,24 @@ public class NotaTabelaView {
             }
         };
 
-        // Preenchendo a tabela a partir do Map
         for (Map<String, String> dados : notas) {
-            model.addRow(new Object[]{
-                dados.get("id"),
-                dados.get("nota"),
-                dados.get("id_diario"),
-                dados.get("matricula_aluno")
+            model.addRow(new Object[] {
+                    dados.get("id"),
+                    dados.get("nota"),
+                    dados.get("id_diario"),
+                    dados.get("matricula_aluno")
             });
         }
 
         JTable tabela = new JTable(model);
         tabela.setRowHeight(25);
-
         JScrollPane scrollPane = new JScrollPane(tabela);
 
-        // Exibe em um diálogo
         JOptionPane.showMessageDialog(
-            null,
-            scrollPane,
-            "Lista de Notas",
-            JOptionPane.PLAIN_MESSAGE
+                null,
+                scrollPane,
+                "Lista de Notas",
+                JOptionPane.PLAIN_MESSAGE
         );
     }
 }
